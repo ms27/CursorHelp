fd21c8af899fd12e532a635ae21b0a816fd808a9
Magomedov Said <linpark1999@gmail.com>
2014-08-26 13:59:57 +0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
  $(window).scroll(function(){
   var wTop=$(this).scrollTop();
   $('.content article').each(function(){
-   var aPos $(this).offset.top();
+   var aPos=$(this).offset.top();
         if(wTop>articlePos){
           $(this).addClass('active');
         }
