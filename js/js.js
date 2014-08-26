c4c3334dbb19f6120643dba368c75dd233c04444
Magomedov Said <linpark1999@gmail.com>
2014-08-26 13:53:53 +0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.content article').each(function(){
    var thisA=$(this),
-    articlePos=thisA.offset.top;
+    articlePos=thisA.offset.top();
       $(window).scroll(function(){
         var wTop=$(this).scrollTop();
         if(wTop>articlePos){
