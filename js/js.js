47fbbfae20ea323b1bf251ea72e31ab03c325204
Magomedov Said <linpark1999@gmail.com>
2014-08-26 15:36:09 +0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+ var headerH=$('header').height();
  $(window).scroll(function(){
   var wTop=$(this).scrollTop(),
   wHeight=$(this).height();
@@ -6,7 +7,7 @@ $(document).ready(function(){
    var aPos=$(this).offset().top,
    aHeight=$(this).height(),
    aLink=$(this).attr('id');
-        if(wTop+$('header').height()>aPos && wTop+$('header').height()<aPos+aHeight){
+        if(wTop+headerH>aPos && wTop+headerH<aPos+aHeight){
          $('[data-active="'+aLink+'"]').addClass('active');
         }
         else{
@@ -23,6 +24,6 @@ $(document).ready(function(){
  $('[data-active]').click(function(){
   var aScroll=$(this).attr('data-active'),
   aScrollPos=$('#'+aScroll).offset().top;
-  $('html, body').animate({scrollTop: aScrollPos}, 300);
+  $('html, body').animate({scrollTop: aScrollPos+headerH}, 300);
  });
 });
