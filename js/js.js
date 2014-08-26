de400c74253f267e96a8f5245751a6f2ca76832a
Magomedov Said <linpark1999@gmail.com>
2014-08-26 13:50:27 +0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
-  if($.browser.webkit){
-    
-    
-  }
-  
+  $('.content article').each(function(){
+   var this=$(this),
+    articlePos=this.offset.top;
+      $(window).scroll(function(){
+        var wTop=$(this).scrollTop();
+        if(wTop>articlePos){
+          this.addClass('active');
+        }
+      });
+  });
 });
