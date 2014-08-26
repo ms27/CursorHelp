1503c5b60c0cb406bade6093840e849ee6061d5d
Magomedov Said <linpark1999@gmail.com>
2014-08-26 15:37:34 +0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
    var aPos=$(this).offset().top,
    aHeight=$(this).height(),
    aLink=$(this).attr('id');
-        if(wTop+headerH>aPos && wTop+headerH<aPos+aHeight){
+        if(wTop+headerH>=aPos && wTop+headerH<=aPos+aHeight){
          $('[data-active="'+aLink+'"]').addClass('active');
         }
         else{
          $('[data-active="'+aLink+'"]').removeClass('active');
         }
-        if(wTop+$('header').height()+wHeight/2>aPos && wTop+$('header').height()+wHeight/2<aPos+aHeight){
+        if(wTop+$('header').height()+wHeight/2>=aPos && wTop+$('header').height()+wHeight/2<=aPos+aHeight){
          $('[data-active="'+aLink+'"]').addClass('active1');
         }
         else{
@@ -24,6 +24,6 @@ $(document).ready(function(){
  $('[data-active]').click(function(){
   var aScroll=$(this).attr('data-active'),
   aScrollPos=$('#'+aScroll).offset().top;
-  $('html, body').animate({scrollTop: aScrollPos+headerH}, 300);
+  $('html, body').animate({scrollTop: aScrollPos-headerH}, 500);
  });
 });
