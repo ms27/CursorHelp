f8c9bb4cae72b15d88c608ab6554944ce4699b32
Magomedov Said <linpark1999@gmail.com>
2014-08-26 13:53:33 +0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $('.content article').each(function(){
    var thisA=$(this),
-    articlePos=this.offset.top;
+    articlePos=thisA.offset.top;
       $(window).scroll(function(){
         var wTop=$(this).scrollTop();
         if(wTop>articlePos){
-          this.addClass('active');
+          thisA.addClass('active');
         }
       });
   });
