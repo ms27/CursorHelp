87f073da5912a3bcdb585d9ea51c961de4bdf7fd
Magomedov Said <linpark1999@gmail.com>
2014-08-26 14:18:31 +0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -3,8 +3,9 @@ $(document).ready(function(){
   var wTop=$(this).scrollTop(),
   wHeight=$(this).height();
   $('.content article').each(function(){
-   var aPos=$(this).offset().top;
-        if(wTop+$('header').height()+wHeight/2>aPos){
+   var aPos=$(this).offset().top,
+   aHeight=$(this).height();
+        if(wTop+$('header').height()+wHeight/2>aPos && wTop+$('header').height()+wHeight/2<aPos+aHeight){
          $(this).addClass('active');
         }
         else{
